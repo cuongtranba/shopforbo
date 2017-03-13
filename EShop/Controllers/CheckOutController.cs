59db8bed5f78af2db0d0054353acb75d3cbe2327
cuongtranba <bacuongtr@gmail.com>
2017-03-13 20:37:45 +0700

    push data to checkout controller

---
+++ EShop/Controllers/CheckOutController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using EShop.Models;
+
+namespace EShop.Controllers
+{
+    public class CheckOutController : Controller
+    {
+        // GET: CheckOut
+        [HttpPost]
+        public ActionResult Index(Order order)
+        {
+            return View();
+        }
+    }
+}
